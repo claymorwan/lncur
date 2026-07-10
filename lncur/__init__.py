da9b0da0159b1fa321ce2cf5c8e5cc1ac3fa2c4a
claymorwan <claymorwan@fembois.dev>
2026-07-10 15:06:06 +0000

    chore(nix): update flake and devenv lockfiles

--- lncur/__init__.py
+++ lncur/__init__.py
@@ -1,19 +1,23 @@
-import argparse, importlib.metadata
+import argparse
+import importlib.metadata
 from lncur.utils.link import link
 from lncur.utils.make import make
 
 def main() -> None:
-    # Parser
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--version", help="Prints version",action="store_true")
-    parser.add_argument("-l", "--link", help="Symlinks cursors files",action="store_true")
-    parser.add_argument("-m", "--make", help="Make a cursor theme directory",type=str)
+  # Parser
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-v", "--version", help="Prints version", action="store_true")
+  parser.add_argument("-l", "--link", help="Symlinks cursors files", action="store_true")
+  parser.add_argument("-m", "--make", help="Generate a cursor theme template", type=str)
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    if args.version:
-        print(f"Lncur v{importlib.metadata.version("lncur")}")
-    if args.link:
-        link()
-    if args.make:
-        make(args.make)
+  if args.version:
+    print(f"Lncur v{importlib.metadata.version("lncur")}")
+  elif args.link:
+    link()
+  elif args.make:
+    # make(args.make)
+    print(args.make)
+  else:
+    parser.print_help()
